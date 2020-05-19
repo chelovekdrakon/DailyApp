0b594bbe9039de74bddb51c32de24281c4bf4eda
Morau Fiodar <MorauFiodar@yahoo.com>
2020-05-19 10:03:48 +0300

    hotfix: logging / planning functionality

--- Daily/controllers/logger/LoggerMasterViewController.m
+++ Daily/controllers/logger/LoggerMasterViewController.m
@@ -316,14 +316,14 @@
     NSManagedObjectContext *context = self.persistentContainer.viewContext;
     NSEntityDescription *entity = [NSEntityDescription entityForName:CD_ENITY_NAME_DAILY inManagedObjectContext:context];
     
-    NSDate *now = [NSDate date];
-    
-    // If it is today
-    if ([[NSCalendar currentCalendar] isDate:daily.date inSameDayAsDate:now]) {
-        Daily *daily = [NSEntityDescription insertNewObjectForEntityForName:CD_ENITY_NAME_DAILY inManagedObjectContext:context];
-        daily.date = [NSDate dayAfterDate:now];;
-        return daily;
-    }
+//    NSDate *now = [NSDate date];
+//
+//    // If it is today
+//    if ([[NSCalendar currentCalendar] isDate:daily.date inSameDayAsDate:now]) {
+//        Daily *daily = [NSEntityDescription insertNewObjectForEntityForName:CD_ENITY_NAME_DAILY inManagedObjectContext:context];
+//        daily.date = [NSDate dayAfterDate:now];;
+//        return daily;
+//    }
         
     // Next Daily request (if exists already)
     NSFetchRequest *request = [[NSFetchRequest alloc] init];
